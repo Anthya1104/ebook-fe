2b9b990abc21e34213e8ee8e30d701029bca3a30
Aaron <ysjhs00189@gmail.com>
2022-09-01 14:44:42 +0800

    [update]樣式

--- ebook-fe/src/index.js
+++ ebook-fe/src/index.js
@@ -1,6 +1,6 @@
 import './index.scss'
 
-import App from './App'
+import AppOrderTest from './AppOrderTest'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import reportWebVitals from './reportWebVitals'
@@ -8,7 +8,7 @@ import reportWebVitals from './reportWebVitals'
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   // <React.StrictMode>
-  <App />
+  <AppOrderTest />
   // </React.StrictMode>
 )
 
