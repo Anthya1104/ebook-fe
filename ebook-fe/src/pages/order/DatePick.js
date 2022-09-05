c33b7c80e022a16e5d49dc34799b4a7d18d9051f
Aaron <ysjhs00189@gmail.com>
2022-09-05 14:03:06 +0800

    [update]order

---
+++ ebook-fe/src/pages/order/DatePick.js
@@ -0,0 +1,10 @@
+function DatePick(){
+    const [date, setDate]=useState();
+
+    console.log
+}
+
+<div className="main">
+        <h1>Selected Date : {date}</h1>
+        <input type="date" onChange={e->setDate(e.target.value)}/>
+      </div>
