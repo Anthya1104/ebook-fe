46ccd6e27460ffad41783f5696c0480f830341ac
Aaron <ysjhs00189@gmail.com>
2022-09-05 14:03:06 +0800

    [update]order

--- ebook-fe/src/pages/order/OrderApp.js
+++ ebook-fe/src/pages/order/OrderApp.js
@@ -1,5 +1,5 @@
+import { useState } from 'react'
 import './OrderApp.css'
-import * as React from 'react'
 import Card from '@mui/material/Card'
 import CardActions from '@mui/material/CardActions'
 import CardContent from '@mui/material/CardContent'
@@ -8,8 +8,154 @@ import Button from '@mui/material/Button'
 import Typography from '@mui/material/Typography'
 
 function OrderApp() {
+  const [date, setDate] = useState()
+  console.log('Date', date)
   return (
     <>
+      <Card sx={{ maxWidth: 1067, maxheight: 400 }} className="mb-3">
+        <div className="row g-0">
+          <div className="col-md-4">
+            <CardMedia
+              className="img-fluid"
+              component="img"
+              height="140"
+              image="/static/images/cards/contemplative-reptile.jpg"
+              alt="book"
+            />
+          </div>
+          <CardContent className="col-md-8">
+            <Typography variant="body2" color="text.secondary">
+              訂單編號
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              訂單狀態
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              訂單日期
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              商品數量
+            </Typography>
+            <Typography
+              gutterBottom
+              variant="h5"
+              component="div"
+              className="border-bottom"
+            >
+              尚未評價
+            </Typography>
+            <Typography
+              gutterBottom
+              variant="h5"
+              component="div"
+              className="border-bottom"
+            >
+              訂單總額
+            </Typography>
+          </CardContent>
+          <CardActions className="justify-content-center">
+            <a href="#" className="btn btn-primary ">
+              完整訂單
+            </a>
+          </CardActions>
+        </div>
+      </Card>
+      <Card sx={{ maxWidth: 1067, maxheight: 400 }} className="mb-3">
+        <div className="row g-0">
+          <div className="col-md-4">
+            <CardMedia
+              className="img-fluid"
+              component="img"
+              height="140"
+              image="/static/images/cards/contemplative-reptile.jpg"
+              alt="book"
+            />
+          </div>
+          <CardContent className="col-md-8">
+            <Typography variant="body2" color="text.secondary">
+              訂單編號
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              訂單狀態
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              訂單日期
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              商品數量
+            </Typography>
+            <Typography
+              gutterBottom
+              variant="h5"
+              component="div"
+              className="border-bottom"
+            >
+              尚未評價
+            </Typography>
+            <Typography
+              gutterBottom
+              variant="h5"
+              component="div"
+              className="border-bottom"
+            >
+              訂單總額
+            </Typography>
+          </CardContent>
+          <CardActions className="justify-content-center">
+            <a href="#" className="btn btn-primary ">
+              完整訂單
+            </a>
+          </CardActions>
+        </div>
+      </Card>
+      <Card sx={{ maxWidth: 1067, maxheight: 400 }} className="mb-3">
+        <div className="row g-0">
+          <div className="col-md-4">
+            <CardMedia
+              className="img-fluid"
+              component="img"
+              height="140"
+              image="/static/images/cards/contemplative-reptile.jpg"
+              alt="book"
+            />
+          </div>
+          <CardContent className="col-md-8">
+            <Typography variant="body2" color="text.secondary">
+              訂單編號
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              訂單狀態
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              訂單日期
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              商品數量
+            </Typography>
+            <Typography
+              gutterBottom
+              variant="h5"
+              component="div"
+              className="border-bottom"
+            >
+              尚未評價
+            </Typography>
+            <Typography
+              gutterBottom
+              variant="h5"
+              component="div"
+              className="border-bottom"
+            >
+              訂單總額
+            </Typography>
+          </CardContent>
+          <CardActions className="justify-content-center">
+            <a href="#" className="btn btn-primary ">
+              完整訂單
+            </a>
+          </CardActions>
+        </div>
+      </Card>
       <Card sx={{ maxWidth: 1067, maxheight: 400 }} className="mb-3">
         <div className="row g-0">
           <div className="col-md-4">
