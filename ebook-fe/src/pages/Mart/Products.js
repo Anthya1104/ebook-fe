a8ef35bde6a17ac504c06cfab387eee5ba973836
Yusa <anthya1104@gmail.co>
2022-09-06 15:51:59 +0800

    [feat]main merge Market

--- ebook-fe/src/pages/Mart/Products.js
+++ ebook-fe/src/pages/Mart/Products.js
@@ -1,6 +1,18 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
+import '../../style/Mart.scss'
+import Category from './Category/Category'
+import Card from './Card/Card'
+import Breadcrumb from './Breadcrumb'
+import Dropdown from './Dropdown'
+import Select from './Select/Select'
+import TopCategory from './TopCategory/TopCategory'
+import Tabbed from './Tabbed/Tabbed'
+import CartTable from './CartTable/CartTable'
+import ProductPage from './ProductPage/ProductPage'
+import ProductInfo from './ProductPage/ProductInfo'
+
 function Products() {
   return (
     <>
@@ -8,6 +20,16 @@ function Products() {
       <Link className="mx-2" to="/Mart/SearchResult">
         Search
       </Link>
+      <Category />
+      <Card />
+      <Breadcrumb />
+      <Dropdown />
+      <Select />
+      <TopCategory />
+      <Tabbed />
+      <CartTable />
+      <ProductPage />
+      <ProductInfo />
     </>
   )
 }
