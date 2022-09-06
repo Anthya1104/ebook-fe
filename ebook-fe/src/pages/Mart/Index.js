7a37741ebdf3ced5d076486975d692eae3068f87
cocohsieh <qqqfangtwtw@gmail.com>
2022-09-06 14:36:34 +0800

    update

--- ebook-fe/src/pages/Mart/Index.js
+++ ebook-fe/src/pages/Mart/Index.js
@@ -1,7 +1,33 @@
 import React from 'react'
+import '../../style/Mart.scss'
+import Category from './Category/Category'
+import Card from './Card/Card'
+import Breadcrumb from './Breadcrumb'
+import Product from './Product'
+import Dropdown from './Dropdown'
+import Select from './Select/Select'
+import TopCategory from './TopCategory/TopCategory'
+import Tabbed from './Tabbed/Tabbed'
+import CartTable from './CartTable/CartTable'
+import ProductPage from './ProductPage/ProductPage'
+import ProductInfo from './ProductPage/ProductInfo'
 
 function Index() {
-  return <div>Index</div>
+  return (
+    <>
+      <Category />
+      <Card />
+      <Breadcrumb />
+      <Product />
+      <Dropdown />
+      <Select />
+      <TopCategory />
+      <Tabbed />
+      <CartTable />
+      <ProductPage />
+      <ProductInfo />
+    </>
+  )
 }
 
 export default Index
---
+++ ebook-fe/src/pages/Mart/TopCategory/TopCategory.js
@@ -0,0 +1,88 @@
+import React, { Component } from 'react'
+import Slider from 'react-slick'
+
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css'
+import './TopCategory.scss'
+
+export default class Responsive extends Component {
+  render() {
+    var settings = {
+      dots: true,
+      infinite: false,
+      speed: 500,
+      slidesToShow: 6,
+      slidesToScroll: 6,
+      initialSlide: 0,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true,
+          },
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+            initialSlide: 2,
+          },
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+          },
+        },
+      ],
+    }
+    return (
+      <div>
+        <h2> 分類 連結要怎麼連??? </h2>
+        <Slider {...settings}>
+          <div className="innerbox">
+            <img src={require('./briefcase.png')} />
+            <h4>商業理財</h4>
+          </div>
+          <div className="innerbox">
+            <img src={require('./quill-pen.png')} />
+            <h4>文學小說</h4>
+          </div>
+          <div className="innerbox">
+            <img src={require('./worldwide.png')} />
+            <h4>社會科學</h4>
+          </div>
+          <div className="innerbox">
+            <img src={require('./leaf.png')} />
+            <h4>生活風格</h4>
+          </div>
+          <div className="innerbox">
+            <img src={require('./inspiration.png')} />
+            <h4>藝術設計</h4>
+          </div>
+          <div className="innerbox">
+            <img src={require('./atom.png')} />
+            <h4>自然科普</h4>
+          </div>
+          <div className="innerbox">
+            <img src={require('./suitcase.png')} />
+            <h4>旅遊觀光</h4>
+          </div>
+          <div className="innerbox">
+            <img src={require('./heartbeat.png')} />
+            <h4>醫療保健</h4>
+          </div>
+          <div className="innerbox">
+            <img src={require('./planting.png')} />
+            <h4>勵志成長</h4>
+          </div>
+        </Slider>
+      </div>
+    )
+  }
+}
