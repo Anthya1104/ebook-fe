afbe6ad19fcc77907f4eb9ba8f7d607bd50e1afc
Yusa <anthya1104@gmail.co>
2022-09-08 18:08:21 +0800

    [feat]bookshelf WIP

--- ebook-fe/src/pages/Member/Bookshelf/Component/RecentBook.js
+++ ebook-fe/src/pages/Member/Bookshelf/Component/RecentBook.js
@@ -4,10 +4,11 @@ import ArrowRight from '../../../../img/recent_book_arrow_r.svg'
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js'
 import { Doughnut } from 'react-chartjs-2'
 import { useState } from 'react'
+import BtnPrimaryReverse from '../../../../Component/Buttons/BtnPrimaryReverse'
 
 // 動態資料連動方法 -> 使用
 ChartJS.register(ArcElement, Tooltip, Legend)
-let readProcess = 50
+let readProcess = 40
 const newdata = {
   labels: [],
   datasets: [
@@ -37,21 +38,37 @@ function RecentBook() {
   // }, [data])
   return (
     <>
-      <div className="">
+      <div className="position-relative m-5">
         <div className="d-flex justify-content-center align-items-center">
           <img className="img-flip m-2" alt="arrow-l" src={ArrowRight} />
           <div className="recent-book d-flex justify-content-center align-items-center">
             <div className="chartLocation">
-              <Doughnut data={data} />
+            <Doughnut data={data} />
             </div>
           </div>
           <img className="m-2" alt="arrow-r" src={ArrowRight} />
         </div>
-        <div className="recent-book-img p-1">
-          <img className="contain-fit" alt="recent-book" src={BookCover} />
+        <div className="position-absolute top-0 Bookshelf-recent-info">
+          <h5>原子習慣：細微改變帶來巨大成就的實證法則</h5>
+          <h6>詹姆斯‧克利爾</h6>
+        </div>
+        <div className="position-absolute bottom-0 Bookshelf-recent-info">
+          <p>{readProcess + '%'}</p>
+        </div>
+        <div className="position-absolute top-0 d-flex align-items-center">
+          <div className="recent-book-img p-1">
+            <img className="contain-fit" alt="recent-book" src={BookCover} />
+          </div>
+
+          <div className="Bookshelf-recent-btn mx-3">
+            {BtnPrimaryReverse('繼續閱讀')}
+            {BtnPrimaryReverse('寫下評論')}
+            {BtnPrimaryReverse('送給朋友')}
+          </div>
         </div>
       </div>
-      <div>RecentBook</div>
+
+      {/* <div>RecentBook</div> */}
     </>
   )
 }
