12a3a8a783e021716d612b86cd2e3bb6d40dacd0
Vic <vicliu325@gmail.com>
2022-09-08 16:25:05 +0800

    add chatroom

--- ebook-fe/src/pages/Member/Coupon/CouponList.js
+++ ebook-fe/src/pages/Member/Coupon/CouponList.js
@@ -1,14 +1,21 @@
 import React from 'react'
+import {Card} from 'react-bootstrap'
 import { Link } from 'react-router-dom'
+import './CouponList.scss'
 
 function CouponList() {
   const couponId = 1
   return (
     <>
-      <div>Coupon List</div>
-      <Link className="mx-2" to={`${couponId}`}>
-        Coupon Detail
-      </Link>
+      <Card className="coupon-btn" bg="warning" style={{ width: '18rem' }}>
+        <Card.Body>
+          <Card.Title>Primary Card Title</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+        </Card.Body>
+      </Card>
     </>
   )
 }
