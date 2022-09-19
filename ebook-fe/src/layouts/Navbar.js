2e15e2278f9d1f54eb9afcdffcb765b21d617ad4
Jiayu <justine0816123@gmail.com>
2022-09-19 19:07:59 +0800

    [feat]update

--- ebook-fe/src/layouts/Navbar.js
+++ ebook-fe/src/layouts/Navbar.js
@@ -1,6 +1,11 @@
 import '../style/Navbar.scss'
-
 import { NavLink } from 'react-router-dom'
+import Login from '../pages/memberindexcategory/478.png'
+import Favoritelist from '../pages/memberindexcategory/heart.png'
+import Shoppingcart from '../pages/memberindexcategory/shopping-cart.png'
+import Search from '../pages/memberindexcategory/search.png'
+import EbookLogo from '../pages/memberindexcategory/EK-logo-final-2.png'
+
 
 function Navbar() {
   return (
@@ -8,30 +13,77 @@ function Navbar() {
       <div className="navbar justify-content-start">
         <div className="navbar-links d-flex p-2 align-items-center">
           <NavLink to="/">
-            <h4>Home</h4>
+            <img className="ebooklogo" src={EbookLogo} alt="ebooklogo" />
           </NavLink>
+
+          <NavLink className="menu-home" to="/">
+            <h4 className="menu-home mx-4">首頁</h4>
+          </NavLink>
+
+          <h6 className="navbar-separate">|</h6>
+
           <div className="menu d-flex justify-content-start align-items-center mx-2">
-            <NavLink className="mx-2" activeClassName="selected" to="/Login">
-              <p className="">Login</p>
+
+            <NavLink className="menu-list mx-4" to="/Mart">
+              商品分類
             </NavLink>
-            <NavLink className="mx-2" to="/Mart">
-              Mart
+
+            <h6 className="navbar-separate">|</h6>
+
+
+            <NavLink className="menu-list mx-4" to="/member-index">
+              會員專區
             </NavLink>
-            <NavLink className="mx-2" to="/member-index">
-              Member
+
+            <h6 className="navbar-separate">|</h6>
+
+            <NavLink className="menu-list mx-4" to="/Chat">
+              聊天室
             </NavLink>
-            <NavLink className="mx-2" to="/Cart">
-              Shopping Cart
+
+            <h6 className="navbar-separate">|</h6>
+
+            <NavLink className="menu-list mx-4" to="/">
+              關於我們
             </NavLink>
-            <NavLink className="mx-2" to="/Chat">
-              Chat
-            </NavLink>
-            <NavLink className="mx-2" to="/Materials">
+
+            <NavLink className="menu-list mx-2" to="/Materials">
               Material Reference
             </NavLink>
-            <NavLink className="mx-2" to="/Preview">
+
+            <NavLink className="menu-list mx-2" to="/Preview">
               Preview
             </NavLink>
+          </div>
+
+          <div className="menu-2 align-items-center justify-content-end d-flex">
+            {/* <NavLink className="mx-2" to="/Preview">
+              <img className="navbar-search" src={Search} alt="search" />
+            </NavLink> */}
+             
+
+            <div class="searchBox">
+              <input className="searchInput" type="text" name="" placeholder="搜尋"></input>
+              <div className="searchButton" href="#">
+                <img className="navbar-search" src={Search} alt="search" />
+              </div>
+            </div>
+
+            <div className="menu-2-2">
+              <NavLink className="menu-2-icon mx-2" activeClassName="selected" to="/Login">
+                <img className="navbar-login" src={Login} alt="Login" />
+                {/* <p className="">Login</p> */}
+              </NavLink>
+
+              <NavLink className="menu-2-icon mx-2" to="/Preview">
+                <img className="navbar-favoritelist" src={Favoritelist} alt="Favoritelist" />
+              </NavLink>
+
+              <NavLink className="menu-2-icon mx-2" to="/Preview">
+                <img className="navbar-shoppingcart" src={Shoppingcart} alt="shoppingcart" />
+              </NavLink>
+            </div>
+
           </div>
         </div>
       </div>
--- ebook-fe/src/pages/MemberIndex.js
+++ ebook-fe/src/pages/MemberIndex.js
@@ -1,20 +1,190 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import '../pages/Memberindex.scss'
+import MemberPic from './memberindexcategory/member.png'
+import Bar from './memberindexcategory/裝飾bar-1 1.png'
 
 function MemberIndex() {
   return (
     <>
-      <div>OverView</div>
+      <div className="member-header d-flex">
+        <div className="">
+          <img className="member-pic" src={MemberPic} alt="memberpic" />
+        </div>
 
-      <Link className="mx-2" to="/Member/order">
-        Order
-      </Link>
-      <Link className="mx-2" to="/Member/bookshelf">
-        Bookshelf
-      </Link>
-      <Link className="mx-2" to="/Member/coupon">
-        Coupon
-      </Link>
+        <div className="">
+          <ul className="member-information">
+            <li className="">
+              <a>會員姓名 : 王大明</a>
+            </li>
+            <li>
+              <a>會員等級 : 銀卡</a>
+            </li>
+          </ul>
+        </div>
+
+        <div className="">
+          <ul className="member-information">
+            <li className="">
+              <a>會員帳號 : wangdaming</a>
+            </li>
+            <li>
+              <a>加入會員日 : 2022/08/02 </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+
+
+
+      <div className="member-bar">
+        <img className="bar" src={Bar} alt="bar" />
+      </div>
+
+
+      <div className="member-container">
+        <ul className="d-flex row flex-wrap">
+          <div className="row justify-content-center member-category">
+            <li className="member-item col-4 m-5">
+              <Link className="member-word" to="/Member/order">
+                <img
+                  className="member-icon"
+                  src={require('../pages/memberindexcategory/order.png')}
+                />
+                <br />
+                訂單相關查詢{' '}
+              </Link>
+            </li>
+
+            <li className="member-item col-4 m-5">
+              <a className="member-word" href="#">
+                <img
+                  className="member-icon"
+                  src={require('../pages/memberindexcategory/memberedit.png')}
+                />
+                <br />
+                會員資料修改{' '}
+              </a>
+            </li>
+
+            <li className="member-item col-4 m-5">
+              <a className="member-word" href="#">
+                <img
+                  className="member-icon"
+                  src={require('../pages/memberindexcategory/pwchange.png')}
+                />
+                <br />
+                修改密碼{' '}
+              </a>
+            </li>
+          </div>
+
+          <div className="row justify-content-center member-category">
+            <li className="member-item col-4 m-5">
+              <Link className="member-word" to="/Member/bookshelf">
+                <img
+                  className="member-icon"
+                  src={require('../pages/memberindexcategory/bookshelf.png')}
+                />
+                <br />
+                我的專屬書櫃{' '}
+              </Link>
+            </li>
+
+            <li className="member-item col-4 m-5">
+              <a className="member-word" href="#">
+                <img
+                  className="member-icon"
+                  src={require('../pages/memberindexcategory/card.png')}
+                />
+                <br />
+                我的優惠券{' '}
+              </a>
+            </li>
+
+            <li className="member-item col-4 m-5">
+              <a className="member-word" href="#">
+                <img
+                  className="member-icon"
+                  src={require('../pages/memberindexcategory/plan.png')}
+                />
+                <br />
+                我的里程碑{' '}
+              </a>
+            </li>
+          </div>
+
+          <div className="row justify-content-center member-category">
+            <li className="member-item col-4 m-5">
+              <a className="member-word" href="#">
+                <img
+                  className="member-icon"
+                  src={require('../pages/memberindexcategory/pay.png')}
+                />
+                <br />
+                支付綁定{' '}
+              </a>
+            </li>
+
+            <li className="member-item col-4 m-5">
+              <a className="member-word" href="#">
+                <img
+                  className="member-icon"
+                  src={require('../pages/memberindexcategory/inmoney.png')}
+                />
+                <br />
+                儲值{' '}
+              </a>
+            </li>
+
+            <li className="member-item col-4 m-5">
+              <a className="member-word" href="#">
+                <img
+                  className="member-icon"
+                  src={require('../pages/memberindexcategory/Q&A.png')}
+                />
+                <br />
+                常見問題{' '}
+              </a>
+            </li>
+          </div>
+
+          <div className="row justify-content-center member-category">
+            <li className="member-item col-4 m-5">
+              <a className="member-word" href="#">
+                <img
+                  className="member-icon"
+                  src={require('../pages/memberindexcategory/gift.png')}
+                />
+                <br />
+                贈送紀錄{' '}
+              </a>
+            </li>
+
+            <li className="member-item col-4 m-5">
+              <a className="member-word" href="#">
+                <img
+                  className="member-icon"
+                  src={require('../pages/memberindexcategory/notification.png')}
+                />
+                <br />
+                訊息通知紀錄{' '}
+              </a>
+            </li>
+
+            <li className="member-item col-4 m-5">
+              <a className="member-word" href="#">
+                <img
+                  className="member-icon"
+                  src={require('../pages/memberindexcategory/logout.png')}
+                />
+                <br />
+                登出{' '}
+              </a>
+            </li>
+          </div>
+        </ul>
+      </div>
     </>
   )
 }
