d3f9b0aa1c8e0935b5717a34f105655554547adb
Yusa <anthya1104@gmail.co>
2022-09-22 15:40:25 +0800

    [feat] update Navigate success

--- ebook-fe/src/pages/Member/Bookshelf/OwnedBookDetail.js
+++ ebook-fe/src/pages/Member/Bookshelf/OwnedBookDetail.js
@@ -1,10 +1,11 @@
-import React, { useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Navigate, Link, useParams } from 'react-router-dom'
 import axios from 'axios'
 import { API_URL } from '../../../utils/config'
 
 function OwnedBookDetail() {
   const { ownedBookId } = useParams()
+  const [updateDone, setUpdateDone] = useState(false)
 
   // post update Data
   useEffect(() => {
@@ -13,12 +14,15 @@ function OwnedBookDetail() {
         let response = await axios.post(
           `${API_URL}/bookshelf/update-recent-book?id=${ownedBookId}`
         )
+        setUpdateDone(true)
+        // console.log('成功', response)
       } catch (e) {
         console.error(e)
       }
     }
     UpdateBook()
   }, [])
+
   const PreviewNavigator = () => {
     return (
       <>
@@ -30,6 +34,14 @@ function OwnedBookDetail() {
   return (
     <>
       {/* TODO: 在這一頁 update 後 Navigate */}
+
+      {updateDone ? (
+        <>
+          <Navigate to="/Preview" />
+        </>
+      ) : (
+        <></>
+      )}
 
       <div>BookDetail:{ownedBookId}</div>
       <Link className="mx-2" to="/Member/bookshelf">
