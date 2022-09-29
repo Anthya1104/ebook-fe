1d8f6747411ffe4bf45be99de09f8fdeabfee667
lonck <joe22053814@gmail.com>
2022-09-29 20:04:40 +0800

    update

--- ebook-fe/src/pages/Home.js
+++ ebook-fe/src/pages/Home.js
@@ -4,6 +4,11 @@ import Footer from '../layouts/Footer'
 import Slider from './Chat/Slider'
 
 import bgImg from '../img/tumblr_1280-1.jpeg'
+import Author1 from '../img/author/JK.jpeg'
+import Author2 from '../img/author/喬治馬丁.jpeg'
+import Author3 from '../img/author/張嘉佳.jpeg'
+import Author4 from '../img/author/不朽.png'
+import Author5 from '../img/author/tom.jpeg'
 import icon1 from '../img/one.png'
 import icon2 from '../img/two.png'
 import icon3 from '../img/three.png'
@@ -33,31 +38,31 @@ function Home() {
       {/* <h1>Home</h1> */}
       <HomePage />
       <Slider />
-      <div>
+      <div className="body">
         <div className="main">
           <div className="home-container">
             <div
               className="panel active"
-              style={{ background: `url(${bgImg})` }}
+              style={{ background: `url(${Author1})` }}
             >
               <img src={icon1} alt="" />
-              <h2>你阿嬤</h2>
+              <h2 className='author'>J·K·羅琳&nbsp;&nbsp;&nbsp;&nbsp;《&nbsp;哈利波特系列&nbsp;》</h2>
             </div>
-            <div className="panel" style={{ backgroundImage: `url(${bgImg})` }}>
+            <div className="panel" style={{ backgroundImage: `url(${Author2})` }}>
               <img src={icon2} alt="" />
-              <h2>這是你阿嬤</h2>
+              <h2>喬治·R·R·馬丁&nbsp;&nbsp;&nbsp;&nbsp;《&nbsp;冰與火之歌系列&nbsp;》</h2>
             </div>
-            <div className="panel" style={{ backgroundImage: `url(${bgImg})` }}>
+            <div className="panel" style={{ backgroundImage: `url(${Author3})` }}>
               <img src={icon3} alt="" />
-              <h2>還是你阿嬤</h2>
+              <h2>張嘉佳&nbsp;&nbsp;&nbsp;&nbsp;《&nbsp;從你的全世界路過、擺渡人&nbsp;》</h2>
             </div>
-            <div className="panel" style={{ backgroundImage: `url(${bgImg})` }}>
+            <div className="panel" style={{ backgroundImage: `url(${Author4})` }}>
               <img src={icon4} alt="" />
-              <h2>又見你阿嬤</h2>
+              <h2>不朽&nbsp;&nbsp;&nbsp;&nbsp;《&nbsp;所有溫柔都是你的隱喻&nbsp;》</h2>
             </div>
-            <div className="panel" style={{ backgroundImage: `url(${bgImg})` }}>
+            <div className="panel" style={{ backgroundImage: `url(${Author5})` }}>
               <img src={icon5} alt="" />
-              <h2>終究是你阿嬤</h2>
+              <h2>湯姆·克蘭西&nbsp;&nbsp;&nbsp;&nbsp;《&nbsp;獵殺紅色十月號&nbsp;》</h2>
             </div>
           </div>
         </div>
