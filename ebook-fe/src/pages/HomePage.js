c1d7ec507a65e689af4bca7f516167dc60b8449e
Jiayu <justine0816123@gmail.com>
2022-09-29 19:50:14 +0800

    [feat]changing bg

--- ebook-fe/src/pages/HomePage.js
+++ ebook-fe/src/pages/HomePage.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../style/Homepage.scss'
 import Homepagebackground from './memberindexcategory/homepagebackground.png'
 import Books from './memberindexcategory/books.png'
@@ -7,14 +7,40 @@ import Comic from './memberindexcategory/comic.png'
 import Magazine from './memberindexcategory/magazine.png'
 import Audiobook from './memberindexcategory/audiobook.png'
 import { Link } from 'react-router-dom'
+import { FaCompactDisc } from 'react-icons/fa'
 
 function HomePage() {
+  const [homePageBackground, setHomePageBackground] = useState('')
+  const onPointerEnter = (key, e) => {
+    let background = []
+    switch (key) {
+      case 'Book':
+        background = Books
+        break
+      case 'Novel':
+        background = Novel
+        break
+      case 'Comic':
+        background = Comic
+        break
+      case 'Magazine':
+        background = Magazine
+        break
+      case 'AudioBook':
+        background = Audiobook
+        break
+      default:
+        background = Homepagebackground
+        break
+    }
+    setHomePageBackground(background)
+  }
+
   return (
     <>
       <article>
         <div className="section-top-page">
           <div className="top-collection">
-
             <div className="homepage-collections">
               {/* 首頁預設背景 */}
               <div className="homepage-bg">
@@ -51,7 +77,6 @@ function HomePage() {
                 <div className="collection-list">
                   <div className="container">
                     <div className="list">
-                    
                       {/* 書籍 */}
                       <div className="col-item">
                         <Link
@@ -59,13 +84,32 @@ function HomePage() {
                           title="Books"
                           to="/Cart/product-list"
                         >
-                          <span
+                          {/* <span
                             className="rectangle"
                             style={{ background: '#EFD3CE' }}
-                          ></span>
+                          ></span> */}
+                          <div
+                            className="rectangle"
+                            style={{
+                              backgroundColor: '#EFD3CE',
+                              width: '100%',
+                              height: '5px',
+                              position: 'absolute',
+                              bottom: '0',
+                              left: '0',
+                            }}
+                          />
+
                           <div className="item-content">
                             <div className="animated">
-                              <div className="item-title">書籍</div>
+                              <div
+                                className="item-title"
+                                onPointerEnter={(e) => {
+                                  onPointerEnter('Book', e)
+                                }}
+                              >
+                                書籍
+                              </div>
                               <div className="item-text">Books</div>
                             </div>
                           </div>
@@ -79,13 +123,32 @@ function HomePage() {
                           title="Novel"
                           to="/Cart/product-list"
                         >
-                          <span
+                          {/* <span
                             className="rectangle"
                             style={{ background: '#E3BBB1' }}
-                          ></span>
+                          ></span> */}
+                          <div
+                            className="rectangle"
+                            style={{
+                              backgroundColor: '#E3BBB1',
+                              width: '100%',
+                              height: '5px',
+                              position: 'absolute',
+                              bottom: '0',
+                              left: '0',
+                            }}
+                          />
+
                           <div className="item-content">
                             <div className="animated">
-                              <div className="item-title">小說</div>
+                              <div
+                                className="item-title"
+                                onPointerEnter={(e) => {
+                                  onPointerEnter('Novel', e)
+                                }}
+                              >
+                                小說
+                              </div>
                               <div className="item-text">Novel</div>
                             </div>
                           </div>
@@ -99,13 +162,32 @@ function HomePage() {
                           title="Comic"
                           to="/Cart/product-list"
                         >
-                          <span
+                          {/* <span
                             className="rectangle"
                             style={{ background: '#B48C8C' }}
-                          ></span>
+                          ></span> */}
+                          <div
+                            className="rectangle"
+                            style={{
+                              backgroundColor: '#B48C8C',
+                              width: '100%',
+                              height: '5px',
+                              position: 'absolute',
+                              bottom: '0',
+                              left: '0',
+                            }}
+                          />
+
                           <div className="item-content">
                             <div className="animated">
-                              <div className="item-title">漫畫</div>
+                              <div
+                                className="item-title"
+                                onPointerEnter={(e) => {
+                                  onPointerEnter('Comic', e)
+                                }}
+                              >
+                                漫畫
+                              </div>
                               <div className="item-text">Comic</div>
                             </div>
                           </div>
@@ -119,13 +201,32 @@ function HomePage() {
                           title="Magazine"
                           to="/Cart/product-list"
                         >
-                          <span
+                          {/* <span
                             className="rectangle"
                             style={{ background: '#DF9D6E' }}
-                          ></span>
+                          ></span> */}
+                          <div
+                            className="rectangle"
+                            style={{
+                              backgroundColor: '#DF9D6E',
+                              width: '100%',
+                              height: '5px',
+                              position: 'absolute',
+                              bottom: '0',
+                              left: '0',
+                            }}
+                          />
+
                           <div className="item-content">
                             <div className="animated">
-                              <div className="item-title">雜誌</div>
+                              <div
+                                className="item-title"
+                                onPointerEnter={(e) => {
+                                  onPointerEnter('Magazine', e)
+                                }}
+                              >
+                                雜誌
+                              </div>
                               <div className="item-text">Magazine</div>
                             </div>
                           </div>
@@ -139,13 +240,32 @@ function HomePage() {
                           title="Audiobook"
                           to="/Cart/product-list"
                         >
-                          <span
+                          {/* <span
                             className="rectangle"
                             style={{ background: '#C0724A' }}
-                          ></span>
+                          ></span> */}
+                          <div
+                            className="rectangle"
+                            style={{
+                              backgroundColor: '#C0724A',
+                              width: '100%',
+                              height: '5px',
+                              position: 'absolute',
+                              bottom: '0',
+                              left: '0',
+                            }}
+                          />
+
                           <div className="item-content">
                             <div className="animated">
-                              <div className="item-title">有聲書</div>
+                              <div
+                                className="item-title"
+                                onPointerEnter={(e) => {
+                                  onPointerEnter('Audiobook', e)
+                                }}
+                              >
+                                有聲書
+                              </div>
                               <div className="item-text">Audiobook</div>
                             </div>
                           </div>
