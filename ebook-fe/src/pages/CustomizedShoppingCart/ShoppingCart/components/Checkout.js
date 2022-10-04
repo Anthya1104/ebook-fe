58bd5ae4e93b9d40660bd53ad7eed5e3ea4022dc
Aaron_Chang <98771629+0xAaronFomo@users.noreply.github.com>
2022-10-04 22:02:28 +0800

    [update]

--- ebook-fe/src/pages/CustomizedShoppingCart/ShoppingCart/components/Checkout.js
+++ ebook-fe/src/pages/CustomizedShoppingCart/ShoppingCart/components/Checkout.js
@@ -26,117 +26,98 @@ function ListItemsWithHook({ tab, handleStep, couponAmount0 }) {
     try {
       const result = await axios.post(`${API_URL}/line/createOrder`)
       console.log(result.data)
-      window.location=result.data.redirect
+      window.location = result.data.redirect
     } catch (error) {
       console.log('error', error)
-    }}
+    }
+  }
 
-    // const onButtonClick = (key, e) => {
-    //   let cTemp = []
-    //   switch (key) {
-    //     case "1":
-    //       return
-    //     break
-    //     case "2":
-    //       return
-    //     break
-    //     case "3":
-    //       return
-    //     break
-    //     default:
-    //       cTemp = []
-    //       break
-    //   }
+  return (
+    <>
+      <table
+        className="table"
+        cellPadding="0"
+        border="0"
+        width="100%"
+        cellSpacing="0"
+      >
+        <thead className="SingleCart-topnav">
+          <tr>
+            <th style={{ width: 200 }} className="SingleCart-tr">
+              商品明細
+            </th>
+            <th></th>
+            <th className="SingleCart-tr2">小計</th>
+          </tr>
+        </thead>
+        <tbody>
+          {items.map((v, i) => {
+            return (
+              <tr key={v.id}>
+                <td>
+                  <img
+                    style={{ width: 120, height: 160 }}
+                    src={v.book_img}
+                    className="card-img-top ProductList-card-img-top"
+                    alt="..."
+                  />
+                </td>
+                <td>{v.book_name} </td>
+                <td>$ {v.price}</td>
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
+      <div className="Checkout-box my-5">
+        <div className="d-flex">
+          <div>
+            <h5 className="ListItemsWithHook-text-em-color ">訂單資訊 </h5>
+            共有&nbsp;
+            <span className="ListItemsWithHook-text-em-color">
+              {cart.totalItems}
+            </span>
+            &nbsp; 件商品
+            <br />
+            訂單小計
+            <span className="ListItemsWithHook-text-em-color">
+              $ {cart.cartTotal}
+            </span>
+            <br />
+            {cart.isEmpty && '購物車為空'}
+          </div>
 
-    return (
-      <>
-        <table
-          className="table"
-          cellPadding="0"
-          border="0"
-          width="100%"
-          cellSpacing="0"
-        >
-          <thead className="SingleCart-topnav">
-            <tr>
-              <th style={{ width: 200 }} className="SingleCart-tr">
-                商品明細
-              </th>
-              <th></th>
-              <th className="SingleCart-tr2">小計</th>
-            </tr>
-          </thead>
-          <tbody>
-            {items.map((v, i) => {
-              return (
-                <tr key={v.id}>
-                  <td>
-                    <img
-                      style={{ width: 120, height: 160 }}
-                      src={v.book_img}
-                      className="card-img-top ProductList-card-img-top"
-                      alt="..."
-                    />
-                  </td>
-                  <td>{v.book_name} </td>
-                  <td>$ {v.price}</td>
-                </tr>
-              )
-            })}
-          </tbody>
-        </table>
-        <div className="Checkout-box my-5">
-          <div className="d-flex">
-            <div className="me-4">
-              <h5 className="me-4 ListItemsWithHook-text-em-color">
-                付款方式{' '}
-              </h5>
-              <input type="radio" value="creditcard" />
-              <label className="py-1"> &nbsp;信用卡</label>
-              <br />
-              <input type="radio" value="linepay" />
-              <label className="py-1"> &nbsp;LINE PAY</label>
-              <br />
-              <input type="radio" value="atm" />
-              <label className="py-1"> &nbsp;ATM 轉帳</label>
+          <div className="">
+            <h5 className="me-4 ListItemsWithHook-text-em-color">付款方式 </h5>
+            <div className="my-1">
+              <button
+                type="button"
+                className="btn btn-primary-reverse ShoppingCart-btn-border-radius"
+                onClick={handleStep(3)}
+              >
+                信用卡支付
+              </button>
+              <button
+                type="button"
+                className="btn btn-primary-reverse ShoppingCart-btn-border-radius"
+                onClick={handleLinePay}
+              >
+                LinePay支付
+              </button>
             </div>
-
-            <div>
-              <h5 className="ListItemsWithHook-text-em-color ">訂單資訊 </h5>
-              共有&nbsp;
-              <span className="ListItemsWithHook-text-em-color">
-                {cart.totalItems}
-              </span>
-              &nbsp; 件商品
-              <br />
-              訂單小計
-              <span className="ListItemsWithHook-text-em-color">
-                $ {cart.cartTotal}
-              </span>
-              <br />
-              {cart.isEmpty && '購物車為空'}
-            </div>
+            {/* <input type="radio" value="creditcard" />
+            <label className="py-1"> &nbsp;信用卡</label>
+            <br />
+            <input type="radio" value="linepay" />
+            <label className="py-1"> &nbsp;LINE PAY</label>
+            <br />
+            <input type="radio" value="atm" />
+            <label className="py-1"> &nbsp;ATM 轉帳</label> */}
           </div>
         </div>
-
-        <div className="d-flex justify-content-end my-5">
-        <button
-            type="button"
-            className="btn btn-primary-reverse ShoppingCart-btn-border-radius"
-            onClick={handleLinePay}
-          >
-            確定付款
-          </button>
-          <button
-            type="button"
-            className="btn btn-primary-reverse ShoppingCart-btn-border-radius"
-            onClick={handleStep(3)} //如何用if else判斷是
-          >
-            確定付款
-          </button>
-        </div>
-      </>
-    )
-  }
+      </div>
+    </>
+  )
+}
 
 export default ListItemsWithHook
